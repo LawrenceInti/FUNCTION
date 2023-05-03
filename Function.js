22c545b51ec3eb7ea038914d10655dc9a10a837e
LawrenceInti <120490082+LawrenceInti@users.noreply.github.com>
2023-05-03 10:24:33 +0530

    Add files via upload

---
+++ Function.js
@@ -0,0 +1,32 @@
+function law(){ //1st answer
+    console.log("Dont Prove Just Improve");
+}
+law();
+
+
+function law(a,b){ //2nd answer
+    pro=(a+b);
+   console.log("sum of a+b is "+ pro); 
+  }
+  law(3,4);
+
+
+  law  = (a,b)=>{ //3rd answer
+    pro=(a+b);
+   console.log("sum of a+b is "+ pro); 
+  }
+  law(3,4);
+
+
+function fact(n){ //7th answer
+        let p=1;
+        for(let i=1; i<=n; i++){
+          p=p*i;
+        }
+        console.log (p);
+      }
+fact(7);
+
+
+
+ 
